dfb3f6c100ed8febeb49523bfa33e1b865d2ac50
romandovhan <dovhan.rg@gmail.com>
2024-02-26 02:53:02 +0200

    refactoring init.js

--- server/init.js
+++ server/init.js
@@ -1,33 +1,43 @@
-
 import {randomUUID} from "node:crypto";
 import {readStateFromFile, writeStateToFile} from "./file.js";
 import {getState} from "./state.js";
 
+/**
+ *
+ * @param {WebSocket} ws WebSocket object
+ * @param obj
+ * @param {type} obj.type incoming message type
+ * @param {id} obj.id  user id
+ * @param {state} [obj.state] user state
+ * @param {selectedCells} [obj.state.selectedCells] location of user ships on field
+ * @param {isBattleStarted} [obj.state.isBattleStarted=boolean] is user located ships and press Start(ready)
+ * @param {userID} [obj.state.userID] user id from localStorage
+ * @return void
+ */
 export const init = async (ws, obj) => {
     await readStateFromFile();
     const state = getState();
-    if ('id' in obj && obj.id in state) {
-        state[obj.id] = {
-            ...state[obj.id],
-            timestamp: new Date(),
-            ...obj,
-        };
-        ws.send(JSON.stringify({
-            type: 'init_resp',
-            status: 'success',
-            id: state[obj.id].id,
-            message: 'updated session timestamp ' + new Date(),}));
-    } else {
-        const id = randomUUID();
+    const {id, state: userState} = obj;
+    if (id && id in state) {
         state[id] = {
-            timestamp: new Date(),
-            id,
-            ...obj,
+            ...state[id],
+            isBattleStarted: userState?.isBattleStarted ?? false,
+            selectedCells: userState?.selectedCells ?? {},
         };
         ws.send(JSON.stringify({
             type: 'init_resp',
             status: 'success',
             id,
+        }));
+    } else {
+        const newID = randomUUID();
+        state[newID] = {
+            id: newID,
+        };
+        ws.send(JSON.stringify({
+            type: 'init_resp',
+            status: 'success',
+            id: newID,
             message: 'initialised session id'
         }));
     }
